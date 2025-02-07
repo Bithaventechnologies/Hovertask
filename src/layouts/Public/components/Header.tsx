63895d67e8380ece2aae4991fe1778969e579139
allAboutJS <victoronah1914@gmail.com>
2025-02-07 14:51:09 +0100

    Refactor Header component to use dynamic navigation links and improve accessibility

--- src/layouts/Public/components/Header.tsx
+++ src/layouts/Public/components/Header.tsx
@@ -2,29 +2,39 @@ import logo from "../../../assets/brand-logo.svg";
 import { RiMenu2Line } from "react-icons/ri";
 import { MdClose } from "react-icons/md";
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+import navLinks from "../lib/navLinks";
 
 const Header = () => {
     const [menuOpen, setMenuOpen] = useState(false);
+    const { pathname } = useLocation();
 
     return (
         <div className="bg-gradient-to-r sticky top-0 w-full left-0 z-50 from-blue-50 to-purple-100 shadow-md">
-            <div className="max-w-7xl mx-auto px-6 py-4 flex justify-between items-center">
+            <div className="max-w-screen-xl mx-auto px-6 py-4 flex justify-between items-center">
                 {/* Logo */}
-                <div>
+                <Link to="/">
                     <img src={logo} alt="Hover Task Logo" className="w-24" />
-                </div>
+                </Link>
 
                 {/* Desktop Navigation */}
-                <nav role="navigation" className="hidden md:flex space-x-10">
-                    {["Home", "Market Place", "About Us", "FAQ", "Contact Us"].map((item) => (
-                        <a
-                            key={item}
-                            href="#"
-                            className="text-gray-800 hover:text-blue-600 transition-all duration-300 text-lg font-medium"
+                <nav role="navigation" className="hidden md:flex space-x-10 text-sm items-center">
+                    {navLinks.map((item) => (
+                        <Link
+                            to={item.path}
+                            key={item.label}
+                            className={`${
+                                pathname === item.path ? "text-blue-600" : "text-gray-800 hover:text-blue-600"
+                            }  transition-colors`}
                         >
-                            {item}
-                        </a>
+                            {item.label}
+                            {pathname === item.path && (
+                                <span className="flex gap-1 justify-center max-w-[90%] mx-auto">
+                                    <span className="h-1 w-2 bg-gradient-to-r from-purple-700 to-blue-600 rounded-full"></span>
+                                    <span className="h-1 bg-gradient-to-r from-purple-700 to-blue-600 rounded-full flex-1"></span>
+                                </span>
+                            )}
+                        </Link>
                     ))}
                 </nav>
 
