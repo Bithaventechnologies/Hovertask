e9a8c30f17c6e8bf6fb20773a3bcba7e8801b2c9
Dhanielgeek <dhanielknightz46@gmail.com>
2025-02-18 07:23:08 +0100

    pushed..

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import Contactus from "./pages/index/components/Contactus";
 import Signup from "./auth/Signup";
 import SignIn from "./auth/Signin";
 import MarketPlace from "./pages/dashboard/components/MarketPlace";
+import Faq from "./pages/index/components/Faq";
 
 const App = () => {
   return (
@@ -19,6 +20,7 @@ const App = () => {
           <Route path="signup" element={<Signup />} />
           <Route path="signin" element={<SignIn />} />
           <Route path="market" element={<MarketPlace />} />
+          <Route path="faq" element={<Faq />} />
         </Route>
         {/* Public routes */}
       </Routes>
